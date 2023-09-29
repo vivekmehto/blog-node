b593ead134086b6284e0fc18d52bff1086b28d30
vivekmehto <vivekmehto.webdev@gmail.com>
2023-09-29 19:10:04 +0530

    ADD: redux store & Auth slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import conf from "./conf/conf";
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout(userData));
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return !isLoading ? (
     <>
       <h1>BlogNode with Appwrite</h1>
     </>
-  );
+  ) : null;
 }
 
 export default App;
