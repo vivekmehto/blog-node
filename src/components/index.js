96683e226b6aaefbb6cd73b32f86b768315624e5
vivekmehto <vivekmehto.webdev@gmail.com>
2023-10-04 19:20:15 +0530

    ADD: Header Component

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Footer from "./Footer/Footer";
+import Button from "./Button";
+import Input from "./Input";
+import Container from "./Container/Container";
+
+export { Header, Footer, Button, Input, Container, LogoutBtn };
