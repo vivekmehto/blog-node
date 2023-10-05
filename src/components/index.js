e5ce87ed342c4d5bd5d39cbf06ae7ecb6f065a2d
vivekmehto <vivekmehto.webdev@gmail.com>
2023-10-05 13:21:38 +0530

    ADD: Sign up Component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,22 @@ import Footer from "./Footer/Footer";
 import Button from "./Button";
 import Input from "./Input";
 import Container from "./Container/Container";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Logo from "./Logo";
 
-export { Header, Footer, Button, Input, Container, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Button,
+  Input,
+  Container,
+  LogoutBtn,
+  Select,
+  Signup,
+  Login,
+  PostCard,
+  Logo,
+};
